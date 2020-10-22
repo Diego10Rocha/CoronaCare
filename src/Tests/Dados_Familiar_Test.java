23dd23269d3dfebca30d84b09366f4e175952177
Diego10Rocha <diego93rocha@gmail.com>
2020-10-22 14:59:03 -0300

    adicionando testes

---
+++ src/Tests/Dados_Familiar_Test.java
@@ -0,0 +1,52 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+
+import Dados.Familiar;
+import Persistencia.FamiliarDAO;
+
+
+@TestMethodOrder(OrderAnnotation.class)
+class Dados_Familiar_Test {
+
+	@Test
+	@Order(1)
+	void cadastrarTest() {
+		Familiar f = new Familiar();
+		f.setNomeUsuario("TESTE");
+		f.setData_Nascimento("2020-02-01");
+		f.setSenhaUsuario("123");
+		f.setEmailUsuario("TESTE@teste.com");
+		f.setTelefoneUsuario("122345678900");
+		boolean expected = f.cadastrar(f);
+		assertTrue(expected);
+	}
+
+	@Test
+	@Order(2)
+	void loginTeste() throws SQLException {
+		boolean expected;
+		Familiar paciente = FamiliarDAO.loginFamiliar("TESTE@teste.com", "123");
+		if (paciente instanceof Familiar) {
+			expected = true;
+		}else {
+			expected = false;
+		}
+		assertTrue(expected);
+	}
+	
+	@Test
+	@Order(3)
+	void deletarTest() throws SQLException {
+		int id = Familiar.getIdFamiliarByEmail("TESTE@teste.com");
+		boolean expected = FamiliarDAO.deleteFamiliar(id);
+		assertTrue(expected);
+	}
+}
---
+++ src/Tests/Dados_Paciente_Test.java
@@ -0,0 +1,55 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import Dados.Paciente;
+import Persistencia.PacienteDAO;
+
+
+@TestMethodOrder(OrderAnnotation.class)
+class Dados_Paciente_Test {
+
+
+	@Test
+	@Order(1)
+	void cadastrarTest() {
+		Paciente p = new Paciente();
+		p.setNomeUsuario("Teste");
+		p.setNickname("TESTE");
+		p.setNomeUsuario("teste");
+		p.setData_Nascimento("2020-02-01");
+		p.setSenhaUsuario("123");
+		p.setEmailUsuario("Teste@teste.com");
+		p.setTelefoneUsuario("122345678900");
+		boolean expected = p.cadastrar(p);
+		assertTrue(expected);
+	}
+	
+	@Test
+	@Order(2)
+	void loginTeste() throws SQLException {
+		boolean expected;
+		Paciente paciente = PacienteDAO.loginPaciente("Teste@teste.com", "123");
+		if (paciente instanceof Paciente) {
+			expected = true;
+		}else {
+			expected = false;
+		}
+		assertTrue(expected);
+	}
+
+	@Test
+	@Order(3)
+	void deletarTest() throws SQLException {
+		int id = Paciente.getPacienteByNickname("TESTE");
+		boolean expected = PacienteDAO.deletePaciente(id);
+		assertTrue(expected);
+	}
+}
