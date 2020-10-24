3507e94a7319e614c49abffdac6107b2e69cf631
stick-s <uefscarlosh@gmail.com>
2020-10-24 12:16:00 -0300

    Corrige indentacao dos arquivos JS

--- WebContent/js/dynamic-profile.js
+++ WebContent/js/dynamic-profile.js
@@ -1,13 +1,13 @@
-function enableAddPatientToFamily(){
-	obj=document.getElementById("form-add-patient-to-family").style;
-	obj.visibility="visible";
-	obj2=document.getElementById("send-message").style;
-	obj2.visibility="hidden";
+function enableAddPatientToFamily() {
+	obj = document.getElementById("form-add-patient-to-family").style;
+	obj.visibility = "visible";
+	obj2 = document.getElementById("send-message").style;
+	obj2.visibility = "hidden";
 }
 
-function sendMessage(){
-	obj2=document.getElementById("send-message").style;
-	obj2.visibility="visible";
-	obj=document.getElementById("form-add-patient-to-family").style;
-	obj.visibility="hidden";
+function sendMessage() {
+	obj2 = document.getElementById("send-message").style;
+	obj2.visibility = "visible";
+	obj = document.getElementById("form-add-patient-to-family").style;
+	obj.visibility = "hidden";
 }
